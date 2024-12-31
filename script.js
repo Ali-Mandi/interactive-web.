815ccc795f0db25d3732c041a6f5a561f1abc5e7
Ali <Dcc7hq@hotmail.com>
2024-12-31 12:15:33 +0400

    Add button and click event functionality

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 window.onload = function () {
     alert("Welcome to the interactive web page!");
 };
+document.addEventListener('DOMContentLoaded', () => {
+    alert('Page has loaded!');
+
+    const button = document.getElementById('clickMe');
+    button.addEventListener('click', () => {
+        alert('Button was clicked!');
+    });
+});
